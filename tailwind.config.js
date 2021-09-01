749938cd83fdb065f79827f15f908f66a4bde60b
Sami Zad <samiraghadirzad@gmail.com>
2021-09-01 09:42:59 +0430

    chnage margin change work section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,29 @@ module.exports = {
     extend: {
       fontFamily: {
         "Sans-Condensed": ['"Open Sans Condensed"', "cursive"],
+        transitionProperty: {
+          width: "width",
+          height: "height",
+        },
       },
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["active"],
+      // ...
+      borderColor: ["focus-visible", "first"],
+      // ...
+      textColor: ["visited"],
+
+      height: ["hover", "focus", "group-hover"],
+      width: ["responsive", "hover", "focus", "group-hover"],
+      transform: ["hover", "focus"],
+      scale: ["active", "group-hover", "hover", "focus"],
+      transitionProperty: ["responsive", "hover", "focus"],
+      transitionTimingFunction: ["responsive", "hover", "focus"],
+      transitionDuration: ["responsive", "hover", "focus"],
+      transitionDelay: ["responsive", "hover", "focus"],
+    },
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
 };
